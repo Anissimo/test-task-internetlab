480563fae627922c2a4fa3ad3ad20479ee8b24a8
grisha <grisaanisimov@gmail.com>
2023-10-20 08:38:18 +0400

    style update

--- js/reviews.js
+++ js/reviews.js
@@ -1,4 +1,4 @@
-import { reviewData } from "./collections.js";
+import { reviewData } from "../public/icons/collections.js";
 
 function createReviewCard(data) {
   var scrollBarContainer = document.querySelector(
@@ -6,26 +6,44 @@ function createReviewCard(data) {
   );
 
   if (scrollBarContainer) {
-    var card = document.createElement("div");
-    card.className = "reviews__scroll-bar--container--card";
+    var cardContainer = document.createElement("div");
+    cardContainer.className = "card-container";
+
+    var imageContainer = document.createElement("div");
+    imageContainer.className = "image-container";
+    cardContainer.appendChild(imageContainer);
+
+    var img = document.createElement("img");
+    img.src = data.icon;
+    img.className = "reviews__scroll-bar--container--card--img";
+    imageContainer.appendChild(img);
+
+    var textContainer = document.createElement("div");
+    textContainer.className = "text-container";
+    imageContainer.appendChild(textContainer);
 
     var nameP = document.createElement("p");
     nameP.textContent = data.name;
-    card.appendChild(nameP);
+    nameP.className = "reviews__scroll-bar--container--card--p1";
+    textContainer.appendChild(nameP);
 
     var locationP = document.createElement("p");
     locationP.textContent = data.location;
-    card.appendChild(locationP);
+    locationP.className = "reviews__scroll-bar--container--card--p2";
+    textContainer.appendChild(locationP);
+
+    var reviewContainer = document.createElement("div");
+    reviewContainer.className = "review-container";
+    cardContainer.appendChild(reviewContainer);
 
     var reviewP = document.createElement("p");
     reviewP.textContent = data.review;
-    card.appendChild(reviewP);
+    reviewP.className = "reviews__scroll-bar--container--card--p3";
+    reviewContainer.appendChild(reviewP);
 
-    scrollBarContainer.appendChild(card);
+    scrollBarContainer.appendChild(cardContainer);
   } else {
-    console.log(
-      'Элемент с классом "reviews__scroll-bar--container--card" не найден'
-    );
+    console.log('Элемент с классом "reviews__scroll-bar--container" не найден');
   }
 }
 
