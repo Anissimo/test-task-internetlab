3e5bc6de80d7a2b9cdbafd74b6b7049e9d1e9495
grisha <grisaanisimov@gmail.com>
2023-10-21 22:02:11 +0400

    style and js reviews update

--- js/reviews.js
+++ js/reviews.js
@@ -1,54 +1,131 @@
 import { reviewData } from "./collections.js";
 
-function createReviewCard(data) {
-  var scrollBarContainer = document.querySelector(
-    ".reviews__scroll-bar--container"
-  );
+let currentIndex = 0; // начальный индекс для отображения карточек
+const cardsToShow = 3; // количество активных карточек
 
-  if (scrollBarContainer) {
+function createReviewCard(data) {
+  var cardsContainer = document.querySelector(".reviews__cards");
+
+  if (cardsContainer) {
     var card = document.createElement("div");
     card.className = "card";
+    card.style.display = "none"; // по умолчанию все карточки скрыты
 
-    var imgContainer = document.createElement("div");
-    imgContainer.className = "img-container";
-    card.appendChild(imgContainer);
+    var imgWrap = document.createElement("div");
+    imgWrap.className = "card__img-wrap";
+    card.appendChild(imgWrap);
 
     var img = document.createElement("img");
     img.src = data.icon;
-    img.className = "card-img";
-    imgContainer.appendChild(img);
+    img.className = "card__img";
+    imgWrap.appendChild(img);
 
-    var textContainer = document.createElement("div");
-    textContainer.className = "text-container";
-    imgContainer.appendChild(textContainer);
+    var textWrap = document.createElement("div");
+    textWrap.className = "card__text-wrap";
+    imgWrap.appendChild(textWrap);
 
     var nameP = document.createElement("p");
     nameP.textContent = data.name;
-    nameP.className = "card-name";
-    textContainer.appendChild(nameP);
+    nameP.className = "card__name";
+    textWrap.appendChild(nameP);
 
-    var locationP = document.createElement("p");
-    locationP.textContent = data.location;
-    locationP.className = "card-location";
-    textContainer.appendChild(locationP);
+    var locP = document.createElement("p");
+    locP.textContent = data.location;
+    locP.className = "card__loc";
+    textWrap.appendChild(locP);
 
-    var reviewContainer = document.createElement("div");
-    reviewContainer.className = "review-container";
-    card.appendChild(reviewContainer);
+    var reviewWrap = document.createElement("div");
+    reviewWrap.className = "card__review-wrap";
+    card.appendChild(reviewWrap);
 
     var reviewP = document.createElement("p");
     reviewP.textContent = data.review;
-    reviewP.className = "card-review";
-    reviewContainer.appendChild(reviewP);
+    reviewP.className = "card__review";
+    reviewWrap.appendChild(reviewP);
 
-    scrollBarContainer.appendChild(card);
+    cardsContainer.appendChild(card);
   } else {
-    console.log('Элемент с классом "reviews__scroll-bar--container" не найден');
+    console.log('Элемент с классом "reviews__cards" не найден');
   }
 }
 
+// функция для обновления видимости карточек
+function updateCardVisibility() {
+  const cards = document.querySelectorAll(".card");
+  cards.forEach((card, index) => {
+    if (index >= currentIndex && index < currentIndex + cardsToShow) {
+      card.style.display = "flex";
+      card.style.animation = "slide-in 1s forwards";
+    } else {
+      card.style.display = "none";
+    }
+  });
+}
+
+// функция для обновления цвета кружков
+function updateCircles() {
+  const circles = document.querySelectorAll("svg circle");
+  circles.forEach((circle, index) => {
+    if (
+      index ===
+      Math.floor((currentIndex + Math.floor(cardsToShow / 2)) / cardsToShow)
+    ) {
+      circle.setAttribute("fill", "#2A6CEA");
+    } else {
+      circle.setAttribute("fill", "#C2C8CD");
+    }
+  });
+}
+
+// функция для создания кружков
+function createCircles() {
+  const svg = document.querySelector("svg");
+  const numberOfCircles = Math.ceil(reviewData.length / cardsToShow);
+
+  for (let i = 0; i < numberOfCircles; i++) {
+    const circle = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "circle"
+    );
+    circle.setAttribute("cx", `${4 + i * 25}`);
+    circle.setAttribute("cy", "4");
+    circle.setAttribute("r", "4");
+    circle.setAttribute("fill", "#C2C8CD");
+
+    svg.appendChild(circle);
+  }
+}
+
+// обработчики событий для кнопок
+document.querySelector(".fi-sr-angle-left").addEventListener("click", () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+  } else {
+    currentIndex = reviewData.length - cardsToShow;
+  }
+
+  updateCardVisibility();
+  updateCircles();
+});
+
+document.querySelector(".fi-sr-angle-right").addEventListener("click", () => {
+  if (currentIndex < reviewData.length - cardsToShow) {
+    currentIndex++;
+  } else {
+    currentIndex = 0;
+  }
+
+  updateCardVisibility();
+  updateCircles();
+});
+
+// вызов функции при загрузке страницы
 document.addEventListener("DOMContentLoaded", function () {
   reviewData.forEach(function (data) {
     createReviewCard(data);
   });
+
+  createCircles(); // создаем кружки
+  updateCardVisibility(); // обновляем видимость карточек после их создания
+  updateCircles(); // обновляем цвета кружков после создания карточек
 });
