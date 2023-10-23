9bc9b675ecb916bcdff4a34f55000bc327fcb3eb
grisha <grisaanisimov@gmail.com>
2023-10-24 01:03:15 +0400

    finished section form

--- js/form.js
+++ js/form.js
@@ -1,13 +1,37 @@
-var input = document.querySelector(".form__input");
-input.addEventListener("input", function () {
-  if (input.type === "text" && input.value.length <= 20) {
-    input.classList.add("valid");
-    input.classList.remove("invalid");
-  } else if (input.type === "tel" && /^[0-9]+$/.test(input.value)) {
-    input.classList.add("valid");
-    input.classList.remove("invalid");
-  } else {
-    input.classList.add("invalid");
-    input.classList.remove("valid");
-  }
+document.addEventListener("DOMContentLoaded", function () {
+  var nameInput = document.querySelector('input[name="name"]');
+  var phoneInput = document.querySelector('input[name="phone"]');
+
+  nameInput.addEventListener("input", function () {
+    var errorMessage = this.parentNode.querySelector(".form__input-error");
+    if (this.value.length > 10) {
+      this.classList.add("invalid");
+      this.classList.remove("valid");
+      errorMessage.style.display = "block";
+    } else if (this.value.length === 0) {
+      this.classList.remove("valid", "invalid");
+      errorMessage.style.display = "none";
+    } else {
+      this.classList.remove("invalid");
+      this.classList.add("valid");
+      errorMessage.style.display = "none";
+    }
+  });
+
+  phoneInput.addEventListener("input", function () {
+    var errorMessage = this.parentNode.querySelector(".form__input-error");
+    var nonNumeric = /[^0-9]/g;
+    if (this.value.match(nonNumeric)) {
+      this.classList.add("invalid");
+      this.classList.remove("valid");
+      errorMessage.style.display = "block";
+    } else if (this.value.length === 0) {
+      this.classList.remove("valid", "invalid");
+      errorMessage.style.display = "none";
+    } else {
+      this.classList.remove("invalid");
+      this.classList.add("valid");
+      errorMessage.style.display = "none";
+    }
+  });
 });
